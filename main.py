7797c33b4c9d74ca88d152713e174cc970b4c661
xElon-Mask <xelonmask@protonmail.com>
2022-09-19 11:37:28 +0200

    generate random string numbers, it's initial sequence

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+0 - Générer une chaine de caractère qui contient 4 chiffres aléatoires, c'est votre séquence initiale.
+
+1 - Ajouter un nouveau nombre aléatoire à la fin de votre séquence
+
+2 - Nettoyer l'écran et affichez "Retenez la séquence" pendant 1 seconde
+
+3 - Afficher la séquence à mémoriser pendant 3 secondes
+
+4 - Nettoyer n'écran et demander la réponse à l'utilisateur
+
+5 - Si la réponse est bonne, afficher pendant 1 seconde "Bonne réponse, votre score est : xxx", puis reboucler à l'étape 1
+
+5bis - Si la réponse n'est pas bonne, sortir du programme et afficher : "Mauvaise réponse, la séquence était : xxxx, votre score final : xxxx"
+"""
+import random
+
+n1 = random.randint(0, 9)
+n2 = random.randint(0, 9)
+n3 = random.randint(0, 9)
+n4 = random.randint(0, 9)
+Initial_Sequence = str(n1) + str(n2) + str(n3) + str(n4)
+print(Initial_Sequence)
