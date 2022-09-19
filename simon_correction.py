724ef24aca055a98bb851cbfe7f766cac9146250
xElon-Mask <xelonmask@protonmail.com>
2022-09-19 14:52:23 +0200

    generating initial number's sequence

--- simon_correction.py
+++ simon_correction.py
@@ -7,3 +7,16 @@ def clear_screen():
         os.system('clear')
     else:
         os.system('cls')
+
+
+chiffre = random.randint(0, 9)
+
+print("Bienvenue dans le jeu du Simon")
+
+# Génération de la séquence initiale
+sequence = ""
+for i in range(4):
+    chiffre = random.randint(0, 9)
+    sequence += str(chiffre)
+
+
